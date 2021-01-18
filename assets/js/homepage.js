8a1dd524bc41b81bd8ce0befa8611dfdc43dcd03
noplur <geoduckphoto@gmail.com>
2021-01-17 16:58:07 -0800

    updated comments

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,4 @@
-// to be executed upon a form submission browser event
+// global variables to be executed upon a form submission browser event
 
 var userFormEl = document.querySelector("#user-form");
 var cityNameEl = document.querySelector("#cityname");
@@ -7,63 +7,64 @@ var currentWeatherContainerEl = document.querySelector("current-weather");
 var forecastContainerEl = document.querySelector("future-weather");
 var APIkey = "d32377506e56284db17e72a06db9c9d8";
 
-
-
 // function for search bar
 
 var formSubmitHandler = function(event) {
-  // prevent page from refreshing
+  
+    // prevent page from refreshing
       event.preventDefault();
 
-// get value from input element
-var cityName = cityNameEl.value.trim();
+    // get value from input element
+    var cityName = cityNameEl.value.trim();
 
-// functions to call when a city name is enterred
+    // functions to call when a city name is enterred
 
-if (cityName) {
-  getCityWeather(cityName);
-  getForecastWeather(cityName);  
-  cityHistory ();
+    if (cityName) {
+    getCityWeather(cityName);
+    getForecastWeather(cityName);  
+    cityHistory ();
 
-  // clear old content
+    // clear old search (city name) content
 
-  cityNameEl.value = "";
+    cityNameEl.value = "";
 
-  // alert when a city name is not typed in
+    // alert when a city name is not typed in
 
-} else {
+    } else {
 
-  alert("Please enter a city");
+    alert("Please enter a city");
 
-}
+    }
 };
 
 // function to get current weather for a city
 
 var getCityWeather = function(cityName) {
+
       // format the Open Weather api url
       var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
   
       // make a request to the url
-    fetch(apiUrl)
-    .then(function(response) {
+      
+      fetch(apiUrl)
+      .then(function(response) {
       // request was successful
       if (response.ok) {
-        response.json().then(function(data) {
+          response.json().then(function(data) {
           displayCityWeather(data, cityName);
         });
 
         // if invalid city name is typed into search bar
-      } else {
+        } else {
         alert("Error: " + response.statusText);
 
-        // removes city name from search history list if invalid city name is typed into search bar
+        // removes city name from city history list if invalid city name is typed into search bar
         var listGroup = document.querySelector(".list-group-item");
         
         listGroup.style.display = 'none';
-      }
+        }
 
-    })
+     })
     .then(function(response) {
 
       // clears current-weather div class so elements will not repeat when another city name is enterred.
@@ -73,7 +74,7 @@ var getCityWeather = function(cityName) {
 
     })
     .catch(function(error) {
-      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      // alert if there is an error with fetch
       alert("Unable to connect to Open Weather");
     });
   };
@@ -82,6 +83,7 @@ var getCityWeather = function(cityName) {
 var displayCityWeather = function(data, city) {
   console.log(data)
 
+  // adds classes and text for display:
   // display city name
   // display current time
   // display icon representation of weather conditions
@@ -96,7 +98,7 @@ var displayCityWeather = function(data, city) {
   var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH")
   var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
 
-  // append city name, temperature, humidity and wind to display on page
+  // append city name, temperature, humidity and wind to display on page, append weather icon to title
 
   title.append(icon)
   weather.append(title)
@@ -104,19 +106,26 @@ var displayCityWeather = function(data, city) {
   weather.append(humidity)
   weather.append(wind)
 
-  // display UV-index
+  // run function to display UV-index
   displayUVIndex(data.coord.lat,data.coord.lon)
 };
 
-// function for UV-Index
+// function for displaying UV-Index
 var displayUVIndex = function (lat, lon) {
+
+  // format the Open Weather api url
   fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIkey}`)
+
     .then(function(response) {
+      // return the response as JSON
       return response.json()
     })
+    // function for what to do with UV-Index data
     .then(function(data) {
       console.log(data)
+      // add a class to the UV-Index data
       var UVtext = $("<p>").addClass("card-text").text("UV Index: " )
+      // add a non-clickable button for the UV-Index for color indicator
       var UVIndex = $("<button>").addClass("btn btn-sm").text(data.value)
 
       // color that indicates whether the UV-index is favorable (1-2), moderate (3-7), or severe (8+)
@@ -130,7 +139,7 @@ var displayUVIndex = function (lat, lon) {
         UVIndex.addClass("btn-danger")
       }
 
-      // append UV-Index to display on page
+    // append UV-Index to display on page
     UVtext.append(UVIndex)
     $(".current-weather").append(UVtext)
     });
@@ -144,8 +153,8 @@ var getForecastWeather = function(cityName) {
   var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
 
   // make a request to the url
-fetch(apiUrl)
-.then(function(response) {
+  fetch(apiUrl)
+  .then(function(response) {
   // request was successful
   if (response.ok) {
     response.json().then(function(data) {
@@ -153,7 +162,7 @@ fetch(apiUrl)
     });
   } 
 })
-.then(function(response) {
+  .then(function(response) {
 
   // clears future-weather div class so elements will not repeat when another city name is enterred. 
 
@@ -161,8 +170,8 @@ fetch(apiUrl)
   forecastContainerEl.innerHTML = '';
   
 })
-.catch(function(error) {
-  // Notice this `.catch()` getting chained onto the end of the `.then()` method
+  .catch(function(error) {
+  // alert if there is an error with fetch
   alert("Unable to connect to Open Weather");
 });
 };
@@ -176,30 +185,29 @@ var displayForecastWeather = function(data, city) {
   $(".row").append(forecastTitle);
 
     // displays 5 separate columns
-  for (i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {
 
-    // creates the columns
+    // creates the columns with royal blue background
     var newCard = $("<div>").attr("class", "col-12 col-md-2 five-day bg-primary text-white rounded-lg p-2");
     $(".future-weather").append(newCard);
 
-  // display date
-  // display icon representation of weather conditions
-  // display temperature
-  // display humidity
+  // display future date
+  // display icon representation of future weather conditions
+  // display future temperature
+  // display future humidity
 
   var title = $("<h4>").addClass("card-header").text(`${moment().add(i + 1, "days").format("M/D/YYYY")}`)
   var temperature = $("<p>").addClass("card-text").text("Temp: " + data.list[0 + i].main.temp +" °F")
   var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.list[0 + i].main.humidity+"%")
   var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[0 + i].weather[0].icon + ".png")
 
-  // append city name, temperature, humidity and wind to display on page
+  // append city name, future temperature and future humidity to display on page
   title.append(icon)
   newCard.append(title)
   newCard.append(temperature)
   newCard.append(humidity)
   }
 };
-
 
 // function to make list of city search history
 function cityHistory () {
@@ -212,8 +220,7 @@ function cityHistory () {
 
   var listItem = $("<li>").addClass("list-group-item list-group-item-action").text(cityName);
 
-    // append listItem to city-list class to display on page
-
+  // prepend listItem to city-list class to display on page
 
   $(".city-list").prepend(listItem);
 
@@ -241,6 +248,6 @@ $(".city-list").click(function(event) {
   
 });
 
-// add event listeners to forms
+// add event listener to forms
 
 userFormEl.addEventListener("submit", formSubmitHandler);
